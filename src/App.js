e20a979cccb21d59ffa72e306cae381055e0caa9
sarathpaul <95843778+sarathpaul@users.noreply.github.com>
2023-08-19 23:37:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from "react";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
